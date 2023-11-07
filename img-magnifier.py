c5036941979878623a82fdc648eb1d9a58a10d0a
Sean Tai <ataiishere34@gmail.com>
2023-11-08 00:26:15 +0800

    move pix_browser to top and use cursor to scroll in scroll_area

--- img-magnifier.py
+++ img-magnifier.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QApplication, QLabel, QScrollArea, QFrame, QGridLayout, QStyle
-from PySide6.QtCore import QRect, Qt, QSize, Signal, QPointF, Slot, QPoint, QEvent
+from PySide6.QtCore import QRect, Qt, QSize, Signal, QPointF, Slot, QPoint, QEvent, QMargins
 from PySide6.QtGui import QPixmap, QCursor, QMouseEvent, QColor, QCursor, QKeyEvent
 import PySide6
 import numpy as np
@@ -8,21 +8,84 @@ from typing import List
 from pix_browser2 import PixBrowser
 
 
-class ImgContainer(QLabel):
-    mouseMoved = Signal(QPoint, QColor, QPixmap, QPoint)
+class ImgBrowser(QLabel):
+    mouseMoved = Signal(QPoint, QColor, QPixmap, QPoint, QPoint)
+    mouseEntered = Signal()
+    mouseLeft = Signal()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.PART_SIZE = 30
+        self._last_mouse_event = None
+        self._viewable_range = 21
+        self.setMouseTracking(True)
 
-    def mouseMoveEvent(self, ev: QMouseEvent) -> None:
-        point = ev.position().toPoint()
+    def set_viewable_range(self, viewable_range: int):
+        self._viewable_range = viewable_range
+
+    def viewable_range(self):
+        return self._viewable_range
+
+    def cursor_safari(self):
+        point = self._last_mouse_event.position().toPoint()
         (x, y) = point.toTuple()
         color = self.pixmap().toImage().pixelColor(x, y)
-        rect = QRect(0, 0, self.PART_SIZE, self.PART_SIZE)
+        rect = QRect(0, 0, self._viewable_range, self._viewable_range)
         rect.moveCenter(point)
         part = self.pixmap().copy(rect)
-        self.mouseMoved.emit(point, color, part, part.rect().center())
+        self._last_mouse_event.scenePosition()
+        return point, color, part, part.rect().center(), self._last_mouse_event.scenePosition().toPoint()
+
+    def mouseMoveEvent(self, ev: QMouseEvent) -> None:
+        self._last_mouse_event = ev
+        self.mouseMoved.emit(*self.cursor_safari())
+
+    def leaveEvent(self, event: PySide6.QtCore.QEvent) -> None:
+        self.mouseLeft.emit()
+
+    def enterEvent(self, event: PySide6.QtGui.QEnterEvent) -> None:
+        self.mouseEntered.emit()
+
+
+class ImgContainer(QScrollArea):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def cursor_scroll(self, point: QPoint):
+        (x, y) = point.toTuple()
+        hb = self.horizontalScrollBar()
+        vb = self.verticalScrollBar()
+        cursor = QCursor()
+        if x < 50 and not hb.isMinimized():
+            hb.setValue(hb.value() - 3)
+            cursor.setPos(cursor.pos() + QPoint(3, 0))
+        elif x > self.width() - 50 - vb.width() and not hb.isMaximized():
+            hb.setValue(hb.value() + 3)
+            cursor.setPos(cursor.pos() + QPoint(-3, 0))
+        if y < 50 and not vb.isMinimized():
+            vb.setValue(vb.value() - 3)
+            cursor.setPos(cursor.pos() + QPoint(0, 3))
+        elif y > self.height() - 50 - hb.height() and not vb.isMaximized():
+            vb.setValue(vb.value() + 3)
+            cursor.setPos(cursor.pos() + QPoint(0, -3))
+
+    def keyPressEvent(self, key_event: PySide6.QtGui.QKeyEvent) -> None:
+        (x, y) = QCursor().pos().toTuple()
+        if key_event.key() == Qt.Key.Key_Up:
+            QCursor().setPos(x, y - 1)
+        elif key_event.key() == Qt.Key.Key_Down:
+            QCursor().setPos(x, y + 1)
+        elif key_event.key() == Qt.Key.Key_Left:
+            QCursor().setPos(x - 1, y)
+        elif key_event.key() == Qt.Key.Key_Right:
+            QCursor().setPos(x + 1, y)
+
+
+class ImgPanel(QWidget):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.img_container = ImgContainer(self)
+        # self.ima
 
 
 class ImgMagnifier(QWidget):
@@ -31,54 +94,50 @@ class ImgMagnifier(QWidget):
         self.widget = self
         self.resize(1000, 500)
 
-        self.label2 = ImgContainer('test2', self)
-        self.label2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label2.setStyleSheet('background-color: #888888')
-        self.label2.setPixmap(QPixmap(r'D:\ATai\Pictures\top_bg.jpg'))
-        self.label2.setMouseTracking(True)
+        self.img_browser = ImgBrowser('test2', self)
+        self.img_browser.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.img_browser.setStyleSheet('background-color: #888888')
+        self.img_browser.setPixmap(QPixmap(r'D:\ATai\Pictures\top_bg.jpg'))
+        self.img_browser.setMouseTracking(True)
+        self.img_browser.mouseMoved.connect(self.update_label)
+        self.img_browser.mouseEntered.connect(self.turn_on_mouse_tracking)
+        self.img_browser.installEventFilter(self)
+        self.img_browser.mouseLeft.connect(self.turn_off_mouse_tracking)
 
-        self.scroll_area = QScrollArea(self)
+        self.scroll_area = ImgContainer(self)
         self.scroll_area.setMinimumSize(QSize(500, 500))
-        self.scroll_area.setWidget(self.label2)
-        self.scroll_area.installEventFilter(self)
+        self.scroll_area.setWidget(self.img_browser)
 
         self.label = QLabel('Position', self)
         self.label.setGeometry(600, 30, 650, 25)
-        self.label2.mouseMoved.connect(self.update_label)
 
         self.label1 = QLabel('Position', self)
         self.label1.setGeometry(600, 60, 200, 200)
 
-        self.frame = PixBrowser(self.label2)
-        self.frame.setGeometry(600, 260, 150, 150)
+        self.frame = PixBrowser(self)
         self.frame.setStyleSheet('background-color: rgb(210,210,210)')
 
     @Slot()
-    def update_label(self, point: QPoint, color: QColor, part: QPixmap, part_center: QPoint):
+    def update_label(self, point: QPoint, color: QColor, part: QPixmap, part_center: QPoint, scene_pos: QPoint):
         self.label.setText(f'{point.toTuple()} {color.getRgb()} {part_center.toTuple()}')
         self.label1.setPixmap(part)
-        self.frame.move(point + QPoint(50, 50))
+        self.frame.move(scene_pos + QPoint(50, 50))
         self.frame.set_pixmap(part, part_center)
+        scroll_area_pos = scene_pos - self.scroll_area.pos()
+        self.scroll_area.cursor_scroll(scroll_area_pos)
 
+    @Slot()
+    def turn_on_mouse_tracking(self):
+        self.setMouseTracking(True)
 
-    def eventFilter(self, watched: PySide6.QtCore.QObject, event: PySide6.QtCore.QEvent) -> bool:
-        if watched == self.scroll_area and event.type() == QEvent.Type.KeyPress:
-            key_event = QKeyEvent(event)
-            (x, y) = QCursor().pos().toTuple()
-            if key_event.key() == Qt.Key.Key_Up:
-                QCursor().setPos(x, y - 1)
-                return True
-            elif key_event.key() == Qt.Key.Key_Down:
-                QCursor().setPos(x, y + 1)
-                return True
-            elif key_event.key() == Qt.Key.Key_Left:
-                QCursor().setPos(x - 1, y)
-                return True
-            elif key_event.key() == Qt.Key.Key_Right:
-                QCursor().setPos(x + 1, y)
-                return True
+    @Slot()
+    def turn_off_mouse_tracking(self):
+        self.setMouseTracking(False)
+        self.frame.resize(0, 0)
 
-        return False
+    def mouseMoveEvent(self, event: PySide6.QtGui.QMouseEvent) -> None:
+        # print(event.pos().toTuple())
+        self.frame.move(event.pos() + QPoint(50, 50))
 
 
 if __name__ == '__main__':
--- pix_browser2.py
+++ pix_browser2.py
@@ -8,6 +8,7 @@ from typing import List
 class PixBrowser(QFrame):
     def __init__(self, *args):
         super().__init__(*args)
+        self.SIZE_N = 200
         self.BORDER_WIDTH = 8
         self.RATIO = 15
         self.label = QLabel(self)
@@ -16,20 +17,24 @@ class PixBrowser(QFrame):
         self.setLineWidth(self.BORDER_WIDTH)
         self.setMidLineWidth(1)
 
+        self.resize(0, 0)
+
     def resizeEvent(self, event: QResizeEvent) -> None:
         (w, h) = event.size().toTuple()
         self.label.resize(w - self.BORDER_WIDTH, h - self.BORDER_WIDTH)
         self.label.move(self.rect().center() - self.label.rect().center())
 
     def set_pixmap(self, pixmap: QPixmap, pixmap_center: QPoint):
-        scaled_pixmap = pixmap.scaled(pixmap.size() * self.RATIO)
-        self.resize(scaled_pixmap.size())
+        ratio = self.SIZE_N // max(*pixmap.size().toTuple())
+        scaled_pixmap = pixmap.scaled(pixmap.size() * ratio)
+
+        self.resize(ratio * max(*pixmap.size().toTuple()), ratio * max(*pixmap.size().toTuple()))
 
         p = QPainter(scaled_pixmap)
         pen = QPen()
-        pen.setColor(QColor(255,0,0))
+        pen.setColor(QColor(255, 0, 0))
         p.setPen(pen)
         (cx, cy) = pixmap_center.toTuple()
-        p.drawRect(QRect(cx * self.RATIO, cy * self.RATIO, self.RATIO, self.RATIO))
+        p.drawRect(QRect(cx * ratio, cy * ratio, ratio, ratio))
         p.end()
         self.label.setPixmap(scaled_pixmap)
